7e3ea9a3e70db75ef66b0789cc59b34eee952e4b
samin20 <samin@uxcam.com>
2020-12-23 15:52:39 +0545

    ios postbuild script added for required frameworks

---
+++ iOS/Editor/UXCamPostBuild.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEditor.Callbacks;
+using UnityEditor.iOS.Xcode;
+
+public class UXCamPostBuild : MonoBehaviour
+{
+    [PostProcessBuild]
+    public static void OnPostprocessBuild(BuildTarget target, string pathToBuiltProject)
+    {
+        if (target != BuildTarget.iOS) return;
+
+        // Initialize PBXProject
+        var projectPath = pathToBuiltProject + "/Unity-iPhone.xcodeproj/project.pbxproj";
+        PBXProject pbxProject = new PBXProject();
+        pbxProject.ReadFromFile(projectPath);
+
+        string targetGuid = pbxProject.GetUnityFrameworkTargetGuid();
+
+        // Add build settings
+        // TODO: Add if required
+
+        // Add frameworks
+        pbxProject.AddFrameworkToProject(targetGuid, "CoreTelephony.framework", false);
+        pbxProject.AddFrameworkToProject(targetGuid, "MobileCoreServices.framework", false);
+        pbxProject.AddFrameworkToProject(targetGuid, "Security.framework", false);
+        pbxProject.AddFrameworkToProject(targetGuid, "WebKit.framework", false);
+
+        pbxProject.WriteToFile(projectPath);
+    }
+}
